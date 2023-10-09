c51e7bff9871839222e5586a333372337b4b991c
Matthew Grenier <mgrenier@email.sc.edu>
2023-10-09 15:16:48 -0400

    sqlite

--- backend/backend/production.py
+++ backend/backend/production.py
@@ -32,7 +32,7 @@ STORAGES = {
     },
 }
 
-# Configure Postgres database based on connection string of the libpq Keyword/Value form
+'''# Configure Postgres database based on connection string of the libpq Keyword/Value form
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
 conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
@@ -44,4 +44,4 @@ DATABASES = {
         'USER': conn_str_params['user'],
         'PASSWORD': conn_str_params['password'],
     }
-}
+}'''
