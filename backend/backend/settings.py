0704752395e063d57164eb213e705c14263fea66
Matthew Grenier <mgrenier@email.sc.edu>
2023-10-08 23:53:03 -0400

    new

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://testbackenddjango.azurewebsites.net', 'testbackenddjango.azurewebsites.net', 'localhost', '127.0.0.1', '20.119.0.51']
+ALLOWED_HOSTS = ['https://testbackenddjango.azurewebsites.net', 'testbackenddjango.azurewebsites.net', 'localhost', '127.0.0.1', ]
 
 
 # Application definition
@@ -116,6 +116,10 @@ USE_I18N = True
 USE_TZ = True
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
