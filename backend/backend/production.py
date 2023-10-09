a9a0d3f85df663aaea454ee1bfdbd9a95eb2cc72
Matthew Grenier <mgrenier@email.sc.edu>
2023-10-09 14:02:45 -0400

    new ip

--- backend/backend/production.py
+++ backend/backend/production.py
@@ -4,7 +4,7 @@ from .settings import BASE_DIR
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'], '169.254.130.4'] if 'WEBSITE_HOSTNAME' in os.environ else ['169.254.130.4']
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 DEBUG = True
 
