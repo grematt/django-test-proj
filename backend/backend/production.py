066c26f5159d4f899bd42c21d231768a53196088
Matthew Grenier <mgrenier@email.sc.edu>
2023-10-09 13:16:41 -0400

    prod database var

--- backend/backend/production.py
+++ backend/backend/production.py
@@ -32,13 +32,15 @@ STORAGES = {
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
 conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
-
-DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=conn_str_params['user'],
-    dbpass=conn_str_params['password'],
-    dbhost=conn_str_params['host'],
-    dbname=conn_str_params['dbname']
-)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': conn_str_params['dbname'],
+        'HOST': conn_str_params['host'],
+        'USER': conn_str_params['user'],
+        'PASSWORD': conn_str_params['password'],
+    }
+}
 
 CORS_ORIGIN_WHITELIST = [
     'https://yellow-sky-0c9a02810.3.azurestaticapps.net/'
