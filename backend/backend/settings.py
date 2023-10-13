5ea05a3e208d21db5e1b485ef1b960da428d5c94
Matthew Grenier <mgrenier@email.sc.edu>
2023-10-13 00:39:00 -0400

    dsfdsfjkds

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,15 +129,15 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://django-test-proj-production.up.railway.app'
+    'https://django-test-proj-production.up.railway.app', '*'
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000', 'https://matthew-ascii-app.up.railway.app'
+    'http://localhost:3000', 'https://matthew-ascii-app.up.railway.app', '*'
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'https://matthew-ascii-app.up.railway.app'
+    'https://matthew-ascii-app.up.railway.app', '*'
 ]
 
 if 'DATABASE_URL' in os.environ:
