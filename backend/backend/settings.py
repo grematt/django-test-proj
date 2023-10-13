7e9ea74324d92dd4d7f12032ef03f6dce5401b24
Matthew Grenier <mgrenier@email.sc.edu>
2023-10-13 00:54:41 -0400

    ahhhh

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,11 +129,20 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://django-test-proj-production.up.railway.app', 'https://matthew-ascii-app.up.railway.app/?', 'https://matthew-ascii-app.up.railway.app'
+    'https://django-test-proj-production.up.railway.app', 'https://matthew-ascii-app.up.railway.app/', 'https://matthew-ascii-app.up.railway.app'
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'https://matthew-ascii-app.up.railway.app'
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
 
 if 'DATABASE_URL' in os.environ:
